c68e34ce5f574cc0aaf343f986a582fd50786650
Tom <tom@bearjam.dev>
2020-05-04 18:28:11 +0100

    time in posts also

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,10 +6,13 @@ import { defaultPresenceProps } from "../animations"
 import { motion } from "framer-motion"
 
 export default function PostTemplate({ frontmatter, children }) {
+  const date = new Date(frontmatter.date).toUTCString().split(' ').slice(1,4).join(' ')
+
   return (
     <Presence key="postTemplate" className={cx("px-2", styles.root)}>
       <motion.div {...defaultPresenceProps}>
         <h1>{frontmatter.title}</h1>
+        <time className="date">{date}</time>
         <article>{children}</article>
       </motion.div>
     </Presence>
