dbbae2c9f4c3358d6b78f41cb417a14b8a9f3385
Tom <tom@bearjam.dev>
2020-05-14 12:17:56 +0100

    middle

--- src/data/blog/flippin-ml-form/FlippinMLForm.js
+++ src/data/blog/flippin-ml-form/FlippinMLForm.js
@@ -8,27 +8,35 @@ const FlippinMLForm = () => {
   })
 
   return (
-    <>
-      <div className={styles.scene}>
-        <div className={styles.card}>
-          <div className={styles.front}>
-            <form className="bg-indigo-500 p-4 shadow-lg rounded-lg">
-              <input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="foo"
-                aria-invalid={errors?.email ? "true" : "false"}
-                aria-describedby="emailError"
-                ref={register}
-              />
-              <input type="submit" />
-            </form>
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <div className={styles.scene}>
+          <div className={styles.text}>
+            <h2>Subscribe to our mailing list</h2>
           </div>
-          <div className={styles.back}></div>
+          <div className={styles.card}>
+            <div className={styles.cardFront}>
+              <form>
+                <div>
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="foo"
+                    aria-invalid={errors?.email ? "true" : "false"}
+                    aria-describedby="emailError"
+                    placeholder="E-mail address"
+                    ref={register}
+                  />
+                  <input type="submit" value="Subscribe" />
+                </div>
+              </form>
+            </div>
+            <div className={styles.cardBack}></div>
+          </div>
         </div>
-      </div>
-    </>
+      </section>
+    </div>
   )
 }
 
