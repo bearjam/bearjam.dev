1322d5e358b1a0b8c60c25a0b26ecb4472b34ac5
Tom <tom@bearjam.dev>
2020-05-19 14:28:12 +0100

    yo

--- src/pages/test-api.js
+++ src/pages/test-api.js
@@ -1,44 +1,41 @@
+import React from "react"
 import { motion } from "framer-motion"
-import React, { useEffect } from "react"
-import useSWR from "swr"
+import { useState } from "react"
+import { AnimatePresence } from "framer-motion"
+import { useForm } from "react-hook-form"
 
-const Wrapper = props => (
+const Foo = props => (
   <motion.div
     animate="animate"
-    initial="initial"
-    exit="initial"
-    variants={{
-      animate: {
-        opacity: 1,
-      },
-      initial: {
-        opacity: 0,
-      },
-    }}
+    exit="exit"
+    initial="exit"
+    variants={{ animate: { opacity: 1 }, exit: { opacity: 0 } }}
     {...props}
   />
 )
 
-const wait = ms => new Promise(res => setTimeout(res, ms))
-
-const APITestPage = () => {
-  const fetcher = url => wait(1000).then(() => fetch(url).then(r => r.json()))
-  const { data, error } = useSWR("/api/bar", fetcher)
-  useEffect(() => {
-    console.log("data", data)
-    console.log("error", error)
-  }, [data, error])
-
-  if (error)
-    return (
-      <Wrapper className="bg-red-500">{JSON.stringify(error, null, 2)}</Wrapper>
-    )
-  if (!data) return <Wrapper className="bg-yellow-500">loading...</Wrapper>
-  return <Wrapper className="bg-green-500">hello {data.name}!</Wrapper>
+const TestAPIPage = () => {
+  const [foo, setFoo] = useState(false)
+  const toggle = () => setFoo(!foo)
+  const { register, handleSubmit } = useForm({ mode: "onSubmit" })
+  const onSubmit = async data => {
+    try {
+      const res = await fetch("/api/bar")
+      console.log("res", res)
+    } catch (err) {
+      console.log("err", err)
+    }
+  }
+  return (
+    <div className="pt-16">
+      <AnimatePresence exitBeforeEnter className="pt-16">
+        <motion.form onSubmit={handleSubmit(onSubmit)}>
+          <input type="email" />
+          <input type="submit" />
+        </motion.form>
+      </AnimatePresence>
+    </div>
+  )
 }
 
-export default () => (
-  <div className="pt-16 bg-pink-500">
-    <APITestPage />
-  </div>
-)
+export default TestAPIPage
